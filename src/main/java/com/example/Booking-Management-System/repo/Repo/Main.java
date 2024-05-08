1d3b552c28098eea6b3ca8fc26730d17e21c533f
Kartik Gupta <112673521+kartikgupta09@users.noreply.github.com>
2024-05-08 13:48:46 +0530

    new changes.java
    New Changes added

--- src/main/java/com/example/Booking-Management-System/repo/Repo/Main.java
+++ src/main/java/com/example/Booking-Management-System/repo/Repo/Main.java
@@ -5,26 +5,21 @@ public class Main {
         CabBookingSystem cabSystem = new CabBookingSystem();
 
         // Onboard users
-        System.out.print(cabSystem.addUser("Abhishek, F, 23")+"\n");
-        System.out.print(cabSystem.addUser("Rahul, M") + "\n");
-        System.out.print(cabSystem.addUser("Rahul, M, 20") + "\n");
+        System.out.print(cabSystem.addUser("Abhishek, M, 23")+"\n");
+        System.out.print(cabSystem.addUser("Rahul, M, 29") + "\n");
         System.out.print(cabSystem.addUser("Nandani, F, 23") + "\n");
-//
+
 ////        // Onboard drivers
         System.out.print(cabSystem.addDriver("Driver1, M, 22", "Swift, KA-01-12345",new int[]{10, 1}) + "\n");
         System.out.print(cabSystem.addDriver("Driver2, M, 29","Swift, KA-01-12345", new int[]{11, 10}) + "\n");
-        System.out.print(cabSystem.addDriver("Driver3, M, 29","Swift, KA-01-12345", new int[]{5, 3}) + "\n");
-        System.out.print(cabSystem.addDriver("Driver4, M, 29","Swift, KA-01-12345", new int[]{14,2}) + "\n");
-//
-//
-//
-        System.out.println(cabSystem.findRide("Abhishek", new int[]{-1, 0}, new int[]{20, 1}));
+        System.out.print(cabSystem.addDriver("Driver3, M, 24","Swift, KA-01-12345", new int[]{5, 3}) + "\n");
 
+        
+        System.out.println(cabSystem.findRide("Abhishek", new int[]{0, 0}, new int[]{20, 1}));
         System.out.println(cabSystem.findRide("Rahul", new int[]{10, 0}, new int[]{15, 3}));
-        System.out.println(cabSystem.findRide("Nandani", new int[]{1, 6}, new int[]{0, 4}));
+        System.out.println(cabSystem.findRide("Nandani", new int[]{15, 6}, new int[]{20, 4}));
 
         List<String> driverList = cabSystem.findRide("Rahul", new int[]{10, 0}, new int[]{15, 3});
-
         System.out.println(cabSystem.chooseRide("Rahul", driverList));
 //        cabSystem.showAvailableDriversAndUsers();
     }
